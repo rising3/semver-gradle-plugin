ee87fe7fbac3f4ff815cb320412aa096e7722ab0
Michio Nakagawa <michio.nakagawa@gmail.com>
2021-03-23 13:37:10 +0900

    feat: improve semver

--- src/test/groovy/com/github/rising3/gradle/semver/SemVerTest.groovy
+++ src/test/groovy/com/github/rising3/gradle/semver/SemVerTest.groovy
@@ -6,21 +6,14 @@ class SemVerTest extends Specification {
     def "Constructor"() {
         expect:
         new SemVer().toString() == '0.0.0'
-    }
-
-    def "Constructor new version"() {
-        expect:
         new SemVer(1,2,3).toString() == '1.2.3'
-    }
+        new SemVer(major, minor, patch, preid, release).toString() == r
 
-    def "Constructor pre-release"() {
-        expect:
-        new SemVer(1,2,3, null, 1).toString() == '1.2.3-1'
-    }
-
-    def "Constructor pre-release with pre-id"() {
-        expect:
-        new SemVer(1,2,3, 'M', 1).toString() == '1.2.3-M.1'
+        where:
+        major | minor | patch | preid | release || r
+        1 | 2 | 3 | null | null | '1.2.3'
+        1 | 2 | 3 | null | 1    | '1.2.3-1'
+        1 | 2 | 3 | 'M'  | 1    | '1.2.3-M.1'
     }
 
     def "to String"() {
@@ -33,91 +26,44 @@ class SemVerTest extends Specification {
         SemVer.parse('1.2.3').getCurrent() == '1.2.3'
     }
 
-    def "Inc major"() {
+    def "Increment version"() {
         expect:
-        SemVer.parse('1.2.3').incMajor().getCurrent() == '2.0.0'
+        SemVer.parse(s).invokeMethod(m, preid)?.toString() == r
+
+        where:
+        s              | m               | preid || r
+        '1.2.3'        | 'incMajor'      | null  || '2.0.0'
+        '1.2.3'        | 'incMinor'      | null  || '1.3.0'
+        '1.2.3'        | 'incPatch'      | null  || '1.2.4'
+        '1.2.3-RC.2'   | 'incPremajor'   | null  || '2.0.0-1'
+        '1.2.3-RC.2'   | 'incPremajor'   | 'RC'  || '2.0.0-RC.2'
+        '1.2.3-RC.2'   | 'incPreminor'   | null  || '1.3.0-1'
+        '1.2.3-RC.2'   | 'incPreminor'   | 'RC'  || '1.3.0-RC.2'
+        '1.2.3-RC.2'   | 'incPrepatch'   | null  || '1.2.4-1'
+        '1.2.3-RC.2'   | 'incPrepatch'   | 'RC'  || '1.2.4-RC.2'
+        '1.2.3-RC.2'   | 'incPrerelease' | null  || '1.2.3-1'
+        '1.2.3-RC.2'   | 'incPrerelease' | 'RC'  || '1.2.3-RC.3'
     }
 
-    def "Inc minor"() {
+    def "Parse version"() {
         expect:
-        SemVer.parse('1.2.3').incMinor().getCurrent() == '1.3.0'
-    }
-
-    def "Inc patch"() {
-        expect:
-        SemVer.parse('1.2.3').incPatch().getCurrent() == '1.2.4'
-    }
-
-    def "Inc pre-major"() {
-        expect:
-        SemVer.parse('1.2.3').incPremajor(null).getCurrent() == '2.0.0-1'
-    }
-
-    def "Inc pre-major with pre-id"() {
-        expect:
-        SemVer.parse('1.2.3-RC.2').incPremajor('RC').getCurrent() == '2.0.0-RC.2'
-    }
-
-    def "Inc pre-minor"() {
-        expect:
-        SemVer.parse('1.2.3-1').incPreminor(null).getCurrent() == '1.3.0-1'
-    }
-
-    def "Inc pre-minor with pre-id"() {
-        expect:
-        SemVer.parse('1.2.3-RC.2').incPreminor('RC').getCurrent() == '1.3.0-RC.2'
-    }
-
-    def "Inc pre-patch"() {
-        expect:
-        SemVer.parse('1.2.3').incPrepatch(null).getCurrent() == '1.2.4-1'
-    }
-
-    def "Inc pre-patch with pre-id"() {
-        expect:
-        SemVer.parse('1.2.3-RC.2').incPrepatch('RC').getCurrent() == '1.2.4-RC.2'
-    }
-
-    def "Inc pre-release"() {
-        expect:
-        SemVer.parse('1.2.3-RC.2').incPrerelease(null).getCurrent() == '1.2.3-1'
-    }
-
-    def "Inc pre-release with pre-id"() {
-        expect:
-        SemVer.parse('1.2.3-RC.2').incPrerelease('RC').getCurrent() == '1.2.3-RC.3'
-    }
-
-    def "parse"() {
-        expect:
-        SemVer.parse('1.2.3').getCurrent() == '1.2.3'
-    }
-
-    def "parse pre-release"() {
-        expect:
-        SemVer.parse('1.2.3-1').getCurrent() == '1.2.3-1'
-    }
-
-    def "parse pre-release with pre-id"() {
-        expect:
-        SemVer.parse('1.2.3-M.1').getCurrent() == '1.2.3-M.1'
-    }
-
-    def "valid"() {
-        expect:
-        Objects.nonNull(SemVer.valid(s)) == r
+        SemVer.parse(s)?.toString() == r
 
         where:
         s               || r
-        '1'             || false
-        '1.'            || false
-        '1.x'           || false
-        '1.x.'          || false
-        '1.x.x-RC.2'    || false
-        '1.x.x-2'       || false
-        '1.0.0'         || true
-        '1.0.0-RC.'     || true
-        '1.0.0-RC.2'    || true
-        '1.0.0-2'       || true
+        '1'             || null
+        '1.'            || null
+        '1.1'           || null
+        '1.1.'          || null
+        '1.1.x-RC.2'    || null
+        '1.1.x-2'       || null
+        '1.2.3-'        || null
+        '1.2.3-RC.'     || null
+        '1.2.3.4'       || null
+        '1.2.3.4-RC.2'  || null
+        '1.2.3.4-2'     || null
+        '1.2.3'         || '1.2.3'
+        '1.2.3-2'       || '1.2.3-2'
+        '1.2.3-RC.2'    || '1.2.3-RC.2'
     }
 }
