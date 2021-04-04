355e688cfdd59a7af9f662bf2eff15f0d49f56a8
Michio Nakagawa <michio.nakagawa@gmail.com>
2021-04-05 01:08:51 +0900

    Polish

--- src/main/groovy/com/github/rising3/gradle/semver/plugins/SemVerGradlePluginExtension.groovy
+++ src/main/groovy/com/github/rising3/gradle/semver/plugins/SemVerGradlePluginExtension.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 rising3 <micho.nakagawa@gmail.com>
+ * Copyright (C) 2021 rising3 <michio.nakagawa@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
